e11fd0c8e621c95bb092b9ba2a880692812d4470
piotr <nwg.piotr@gmail.com>
2025-06-24 00:21:24 +0200

    support launch through uwsm #160

--- tools.go
+++ tools.go
@@ -610,6 +610,16 @@ func launch(command string, terminal bool, terminate bool) {
 		cmd = exec.Command("hyprctl", "dispatch", "exec", strings.Join(elements, " "))
 	} else if *wm == "river" {
 		cmd = exec.Command("riverctl", "spawn", strings.Join(elements, " "))
+	} else if *wm == "uwsm" {
+		var commandArgs []string
+		words := strings.Fields(command)
+		for _, word := range words {
+			commandArgs = append(commandArgs, word)
+		}
+		cmd = exec.Command("uwsm", "app", "--")
+		if commandArgs != nil && len(commandArgs) > 0 {
+			cmd.Args = append(cmd.Args, commandArgs...)
+		}
 	}
 
 	msg := fmt.Sprintf("Executing command: %q; args: %q\n", cmd.Args[0], cmd.Args[1:])
