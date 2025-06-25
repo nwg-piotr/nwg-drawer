7ec34c93e25adbdd61eb48d59dd14e07653bd652
piotr <nwg.piotr@gmail.com>
2025-06-26 00:51:58 +0200

    update -wm check and help

--- main.go
+++ main.go
@@ -153,9 +153,9 @@ func defaultTermIfBlank(s, fallback string) string {
 }
 
 func validateWm() {
-	if !(*wm == "sway" || *wm == "hyprland" || *wm == "Hyprland" || *wm == "uwsm") && *wm != "" {
+	if !(*wm == "sway" || *wm == "hyprland" || *wm == "Hyprland" || *wm == "river" || *wm == "niri" || *wm == "uwsm") && *wm != "" {
 		*wm = ""
-		log.Warn("-wm argument supports only 'sway', 'hyprland', 'river' or 'uwsm' string.")
+		log.Warn("-wm argument can be only 'sway', 'hyprland', 'river', 'niri' or 'uwsm'")
 	}
 }
 
@@ -181,7 +181,7 @@ var itemSpacing = flag.Uint("spacing", 20, "icon spacing")
 var lang = flag.String("lang", "", "force lang, e.g. \"en\", \"pl\"")
 var fileManager = flag.String("fm", "thunar", "File Manager")
 var term = flag.String("term", defaultTermIfBlank(os.Getenv("TERM"), "foot"), "Terminal emulator")
-var wm = flag.String("wm", "", "use swaymsg exec (with 'sway' argument) or hyprctl dispatch exec (with 'hyprland') or riverctl spawn (with 'river') or uwsm app -- (with 'uwsm' for Universal Wayland Session Manager) to launch programs")
+var wm = flag.String("wm", "", "use swaymsg exec (with 'sway' argument) or hyprctl dispatch exec (with 'hyprland') or riverctl spawn (with 'river') or niri msg action spawn -- (with 'niri') or uwsm app -- (with 'uwsm' for Universal Wayland Session Manager) to launch programs")
 var nameLimit = flag.Int("fslen", 80, "File Search name LENgth Limit")
 var noCats = flag.Bool("nocats", false, "Disable filtering by category")
 var noFS = flag.Bool("nofs", false, "Disable file search")
